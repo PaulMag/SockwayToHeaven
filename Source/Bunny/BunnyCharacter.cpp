1a5a4c15c2918e15b906f9571ce37c6606b6ecee
Simon Filip Berg <simonfilipberg@gmail.com>
2018-02-07 12:41:31 +0100

    Initial Setup
    Set up folder structure and added a BunnyCharacter class with some initial framwork c++ code that is still in progress.
    
    Project builds and compiles and runs without crashes at this stage.
    
    Source files builds without errors.

---
+++ Source/Bunny/BunnyCharacter.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BunnyCharacter.h"
+#include "Bunny.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Camera/CameraActor.h"
+#include "Components/InputComponent.h"
+
+
+// Sets default values
+ABunnyCharacter::ABunnyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
+
+	OurVisibleComponent->SetupAttachment(RootComponent);
+
+
+}
+
+// Called when the game starts or when spawned
+void ABunnyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+
+	//PlayerController->SetViewTarget(TopDownCamera);
+
+	PlayerController->bShowMouseCursor = true;
+	PlayerController->bEnableClickEvents = true;
+	PlayerController->bEnableMouseOverEvents = true;
+
+	//InputComponent->BindAxis("MoveX", this, &ABunnyCharacter::Move_XAxis);
+	//InputComponent->BindAxis("MoveY", this, &ABunnyCharacter::Move_YAxis);
+	//InputComponent->BindAxis("RotateZ", this, &ABunnyCharacter::RotateZ);
+	
+}
+
+// Called every frame
+void ABunnyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABunnyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+/*void ABunnyCharacter::Move_XAxis(float AxisValue)
+{
+	CurrentVelocity.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+}
+
+void ABunnyCharacter::Move_YAxis(float AxisValue)
+{
+	CurrentVelocity.Y = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+}
+
+void ABunnyCharacter::RotateZ(float AxisValue)
+{
+	CurrentRotation = FMath::Clamp(AxisValue, -1.0f, 1.0f);
+}*/
---
+++ Source/Bunny/BunnyCharacter.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BunnyCharacter.generated.h"
+
+UCLASS()
+class BUNNY_API ABunnyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABunnyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* OurVisibleComponent;
+
+	UShapeComponent* CollisionCapsule = nullptr;
+
+	//void Move_XAxis(float AxisValue);
+	//void Move_YAxis(float AxisValue);
+	//void RotateZ(float AxisValue);
+
+	//FVector CurrentVelocity;
+	//float CurrentRotation;
+	
+};

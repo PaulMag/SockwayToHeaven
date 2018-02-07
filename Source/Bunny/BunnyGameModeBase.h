14620a446765922efeff69c47bf862dc8ba6c555
Paul Magnus Sørensen-Clark <paulmag91@gmail.com>
2018-02-07 11:42:07 +0100

    initial commit

---
+++ Source/Bunny/BunnyGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BunnyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUNNY_API ABunnyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
